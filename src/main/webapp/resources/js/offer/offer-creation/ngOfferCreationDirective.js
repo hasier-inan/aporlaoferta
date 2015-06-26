e11827cba9baaa9a86cc3ac44183c64bdc7f54eb
Hasier Iñan <hasiermetal@gmail.com>
2015-06-26 19:40:53 +0100

    updated offer_list and fixed image with feedback box

--- src/main/webapp/resources/js/offer/offer-creation/ngOfferCreationDirective.js
+++ src/main/webapp/resources/js/offer/offer-creation/ngOfferCreationDirective.js
@@ -9,20 +9,21 @@ aporlaofertaApp
             scope: {
                 overheadDisplay: '='
             },
-            controller: ['$rootScope','$scope', 'requestManager', 'configService', 'offerCreationService', function ($rootScope,$scope, requestManager, configService, offerCreationService) {
+            controller: ['$rootScope', '$scope', 'requestManager', 'configService', 'offerCreationService', function ($rootScope, $scope, requestManager, configService, offerCreationService) {
                 $scope.offer = {};
                 $scope.createOffer = function () {
                     requestManager.makePostCall($scope.offer, {}, configService.getEndpoint('create.offer'))
                         .success(function (data, status, headers, config) {
                             $rootScope.$broadcast('serverResponse', data);
                         }).error(function (data, status, headers, config) {
-                            var theResponse={};
-                            theResponse.description=data;
-                            theResponse.responseResult="error";
+                            var theResponse = {};
+                            theResponse.description = data;
+                            theResponse.responseResult = "error";
                             $rootScope.$broadcast('serverResponse', theResponse);
                         });
-                    $scope.overheadDisplay=false;
-                    $scope.offer={};
+                    $scope.overheadDisplay = false;
+                    $scope.resetValues($scope.offer.offerCategory);
+
                 };
                 $scope.bigDecimalsOnly = /^\-?\d+((\.|\,)\d+)?$/;
                 $scope.$on('selectedCompany', function (event, args) {
@@ -32,6 +33,12 @@ aporlaofertaApp
                 $scope.$on('selectedCategory', function (event, args) {
                     $scope.offer.offerCategory = args.category;
                 });
+                $scope.resetValues = function (defaultCategory) {
+                    $scope.offer = {};
+                    $scope.brandNewCompany = false;
+                    $scope.offer.offerCategory = defaultCategory;
+
+                }
             }]
         }
     });
