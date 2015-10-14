3d16b8b64534d0f3ebac13cd60f526397b8b1f3d
hasiermetal <hasiermetal@gmail.com>
2015-10-14 17:59:02 +0100

    added update directive, head display includes ng-update-offer with custom specification offer. fixed css for buttons

---
+++ src/main/webapp/resources/js/offer/offer-creation/ngOfferUpdateDirective.js
@@ -0,0 +1,67 @@
+/**
+ * Created by hasiermetal on 16/03/14.
+ */
+aporlaofertaApp
+    .directive('ngOfferCreation', function () {
+        return {
+            restrict: 'A',
+            templateUrl: 'resources/js/offer/offer-creation/offerCreation.html',
+            scope: {
+                overheadDisplay: '=',
+                customCloseCallback: '=',
+                displayCallback: '='
+            },
+            controller: ['$scope', 'offerManager', 'requestManager', 'configService', 'alertService', 'vcRecaptchaService',
+                function ($scope, offerManager, requestManager, configService, alertService, vcRecaptchaService) {
+                    $scope.offer = {};
+                    $scope.publicKey = "6LdqHQoTAAAAAAht2VhkrLGU26eBOjL-nK9zXxcn";
+                    $scope.resetCategory = true;
+                    $scope.createOffer = function () {
+                        if (vcRecaptchaService.getResponse() === "") {
+                            $scope.offerCreationError("Por favor, haga click en el captcha para demostrar que no es un robot");
+                            vcRecaptchaService.reload();
+                        }
+                        else {
+                            $scope.processing=true;
+                            requestManager.makePostCall($scope.offer, {recaptcha: vcRecaptchaService.getResponse()}, configService.getEndpoint('create.offer'))
+                                .success(function (data, status, headers, config) {
+                                    if (!alertService.isAllOk(data)) {
+                                        $scope.offerCreationError(data.descriptionEsp);
+                                    }
+                                    else {
+                                        alertService.sendErrorMessage(data.descriptionEsp);
+                                        $scope.resetValues();
+                                        $scope.customCloseCallback = function () {
+                                            offerManager.requestNewestOffers();
+                                        }
+                                    }
+                                }).error(function (data, status, headers, config) {
+                                    $scope.offerCreationError(alertService.getDefaultMessage());
+                                }).finally(function(){
+                                    $scope.processing=false;
+                                });
+                            //$scope.overheadDisplay = false;
+                        }
+                    };
+
+                    $scope.restartRecaptcha = function () {
+                        vcRecaptchaService.reload();
+                    }
+
+                    $scope.offerCreationError = function (customMessage) {
+                        alertService.sendErrorMessage(customMessage);
+                        $scope.restartRecaptcha();
+                        $scope.customCloseCallback = $scope.displayCallback;
+                    }
+
+                    $scope.bigDecimalsOnly = /^\-?\d+((\.|\,)\d+)?$/;
+                    $scope.resetValues = function () {
+                        $scope.restartRecaptcha();
+                        $scope.offer = {};
+                        $scope.brandNewCompany = false;
+                        $scope.resetCategory = true;
+                        $scope.resetCompany = true;
+                    };
+                }]
+        }
+    });
