644b24380f5c51b2f25957299ea7a6b7e3e798b2
Hasier Iñan <hasiermetal@gmail.com>
2016-06-19 16:53:40 +0100

    Content filter updated to automatic search.

--- src/main/webapp/resources/js/offer/offer-filter/ngOfferFilterDirective.js
+++ src/main/webapp/resources/js/offer/offer-filter/ngOfferFilterDirective.js
@@ -14,14 +14,18 @@ aporlaofertaApp
             controller: ['$scope', '$rootScope', 'requestManager', 'configService', function ($scope, $rootScope, requestManager, configService) {
                 $scope.filter = {};
                 $scope.displayFilterContent = "";
+                $scope.previousCategory = "all";
                 $scope.requestFilterApply = function () {
                     $scope.processing = true;
                     $scope.filter.hot = $scope.selection === 'hottestOffers';
                     if (!$scope.isCategorySelected()) {
                         $scope.filter.selectedcategory = "";
                     }
-                    $scope.offerFilter = $scope.filter;
-                    requestManager.makePostCall($scope.filter, {}, configService.getEndpoint('get.filtered.offers'))
+                    $scope.offerFilter = angular.copy($scope.filter);
+                    if ($scope.offerFilter.text.length < 3) {
+                        $scope.offerFilter.text = "";
+                    }
+                    requestManager.makePostCall($scope.offerFilter, {}, configService.getEndpoint('get.filtered.offers'))
                         .success(function (data, status, headers, config) {
                             $scope.offerList = data.theOffers;
                         }).error(function (data, status, headers, config) {
@@ -33,13 +37,22 @@ aporlaofertaApp
                             $scope.processing = false;
                         });
                 };
+
+                $scope.$watch('filter.selectedcategory', function () {
+                    if ($scope.previousCategory !== $scope.filter.selectedcategory) {
+                        $scope.previousCategory = $scope.filter.selectedcategory;
+                        $scope.requestFilterApply();
+                    }
+                });
+
                 $scope.cleanFilters = function () {
                     $scope.filter.expired = false;
                     $scope.filter.text = "";
                     $scope.resetCategory = true;
                     $scope.filter.selectedcategory = "";
-                    $scope.requestFilterApply();
+//                    $scope.requestFilterApply();
                 };
+
                 $scope.displayFilterContents = function () {
                     $scope.displayFilterContent = ($scope.displayFilterContent == 'filters-displayed') ?
                         'filters-hidden' : 'filters-displayed';
