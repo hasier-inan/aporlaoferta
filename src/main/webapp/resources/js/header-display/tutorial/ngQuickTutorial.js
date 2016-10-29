b0009571de8b27abe71a6f17ad952f77c52fb93d
Hasier Iñan <hasiermetal@gmail.com>
2016-10-29 15:54:07 +0100

    Tutorial and overhead fixed.

--- src/main/webapp/resources/js/header-display/tutorial/ngQuickTutorial.js
+++ src/main/webapp/resources/js/header-display/tutorial/ngQuickTutorial.js
@@ -9,18 +9,25 @@ aporlaofertaApp
                 isDisplayed: '=',
             },
             templateUrl: 'resources/js/header-display/tutorial/quickTutorial.jsp',
-            controller: ['$scope', '$element',
-                function ($scope, $element) {
+            controller: ['$scope', '$element', '$timeout',
+                function ($scope, $element, $timeout) {
                     $scope.shareUrl = "www.aporlaoferta.com";
                     $scope.shareText = $("meta[property='og:description']").attr("content");
 
                     $scope.$watch('isDisplayed', function () {
                         if ($scope.isDisplayed) {
-                            $element.resize();
-                            $element.find('.slick-dots li:first a').trigger('click');
-                            $scope.isDisplayed = 0;
+                            $scope.refreshTutorial();
+                            $timeout(function () {
+                                $scope.refreshTutorial();
+                            }, 300);
                         }
                     });
+
+                    $scope.refreshTutorial = function () {
+                        $element.resize();
+                        $element.find('.slick-dots li:first a').trigger('click');
+                        $scope.isDisplayed = 0;
+                    }
 
 
                 }]
