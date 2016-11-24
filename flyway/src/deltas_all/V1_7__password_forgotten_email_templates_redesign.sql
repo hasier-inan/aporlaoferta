d69e9ec8f546278b7ca289ce8f1b6523d9c26c6c
Hasier Iñan <hasiermetal@gmail.com>
2016-11-24 20:10:22 +0000

    Email template fix for small screens.

--- flyway/src/deltas_all/V1_7__password_forgotten_email_templates_redesign.sql
+++ flyway/src/deltas_all/V1_7__password_forgotten_email_templates_redesign.sql
@@ -5,7 +5,7 @@ SET `EMT_CONTENT` = '<html>
 						</head>
 						<body style=\"color:#ffffff;font-size:16px;\">
 						<div style=\"background-color:#111b24;border-radius: 2em;\">
-							<div style=\"background-color:#394656;border-radius:2em;text-align:left;margin:4em 4em 4em 4em;background-image: url(\'https://s3-us-west-2.amazonaws.com/aporlaofertaimages/static/background_head5.png\');\">
+							<div style=\"background-color:#394656;border-radius:2em;text-align:left;background-image: url(\'https://s3-us-west-2.amazonaws.com/aporlaofertaimages/static/background_head5.png\');\">
 								<div style=\"text-align:center;\">
 									<img style=\"width:100px;height:100px;\"
 										 src=\"https://s3-us-west-2.amazonaws.com/aporlaofertaimages/static/logo2.png\"/>
@@ -21,12 +21,12 @@ SET `EMT_CONTENT` = '<html>
 									puedes ignorar este mensaje.<br/>
 									Para reiniciar tu contraseña de usuario sigue el siguiente enlace:<br/><br/>
 
-									<h2>
+									<h3>
 										<a style=\"margin-top:1em;background-color: #618eb8;padding:.8em;color:#ffffff;border-radius:.5em;border:solid 2px #618eb8;\"
 										   href=\"${server}/passwordForgotten?user=${nickname}&track=${userid}\" target=\"_blank\">
 											Reiniciar contraseña
 										</a>
-									</h2>
+									</h3>
 									<br/>
 
 									<div style=\"text-align:center;margin-left:1em;color:#ffffff;\">
