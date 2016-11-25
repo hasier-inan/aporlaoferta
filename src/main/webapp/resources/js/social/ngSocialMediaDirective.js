d8e35b14fcb2780ea1a3f2d8422abf6b6e6971b3
Hasier Iñan <hasiermetal@gmail.com>
2016-11-25 20:36:15 +0000

    Hotfix for the social media share, including a watch task for the urls.

--- src/main/webapp/resources/js/social/ngSocialMediaDirective.js
+++ src/main/webapp/resources/js/social/ngSocialMediaDirective.js
@@ -12,10 +12,17 @@ aporlaofertaApp
             },
             controller: ['$scope',
                 function ($scope) {
-                    $scope.facebookUrl = "https://www.facebook.com/sharer/sharer.php?u=" + $scope.shareUrl;
-                    $scope.twitterUrl = "https://twitter.com/intent/tweet?url=" +
-                        $scope.shareUrl + "&text=" + encodeURIComponent($scope.shareText); + "&via=aporlaoferta";
-                    $scope.googlePlusUrl = "https://plus.google.com/share?url=" + $scope.shareUrl
+                    $scope.$watch('shareUrl', function () {
+                        $scope.facebookUrl = "https://www.facebook.com/sharer/sharer.php?u=" + $scope.shareUrl;
+                        $scope.twitterUrl = "https://twitter.com/intent/tweet?url=" +
+                            $scope.shareUrl + "&text=" + encodeURIComponent($scope.shareText); + "&via=aporlaoferta";
+                        $scope.googlePlusUrl = "https://plus.google.com/share?url=" + $scope.shareUrl
+                    });
+
+                    $scope.$watch('shareText', function () {
+                        $scope.twitterUrl = "https://twitter.com/intent/tweet?url=" +
+                            $scope.shareUrl + "&text=" + encodeURIComponent($scope.shareText); + "&via=aporlaoferta";
+                    });
                 }]
 
         }
