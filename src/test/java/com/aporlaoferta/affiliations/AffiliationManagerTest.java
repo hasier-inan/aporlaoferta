1ab1014ed9f1775b7fe46acec41e15c9d6282731
Hasier Iñan <hasiermetal@gmail.com>
2016-12-11 13:39:59 +0000

    Affiliation manager amended to include multiple keys and values. Opirata and dx affiliations included.

--- src/test/java/com/aporlaoferta/affiliations/AffiliationManagerTest.java
+++ src/test/java/com/aporlaoferta/affiliations/AffiliationManagerTest.java
@@ -23,11 +23,16 @@ public class AffiliationManagerTest {
     protected AffiliationManager affiliationManager;
     private final String rawUrl = "http://www.amazon.es";
     private final String affiliateId = "aporlaoferta-21";
+    private final String affiliateIdKey = "tag";
     //aporlaoferta-21   amazon.es   Amazon  amazon.es amazon
     private final String longUrlWithMultipleValues = "http://www.amazon.es/gp/product/B00K690INE/" +
             "?aka=2&ref=s9_simh_gw_p23_d12_i4?pf_rd_m=A1AT7YVPFBWXBL&pf_rd_s=desktop-1" +
             "&pf_rd_r=0YP4M8XZTZQRA7VN6Y28&pf_rd_t=36701" +
             "&pf_rd_p=579624687&pf_rd_i=desktop&tag=sdfsdf";
+
+    private final String dxAffiliationIds="17266950 affiliate";
+    private final String dxAffiliationIdKeys="Utm_rid Utm_source";
+    private final String dxRawUrl= "http://www.dx.com/p/fidget-dice-cubic-toy-for-focusing-stress-relieving-black-452239#.WE06HbLJxhF";
 
     @Before
     public void setUp() throws Exception {
@@ -41,21 +46,24 @@ public class AffiliationManagerTest {
         assertTrue(rawUrl.equals(parsedUrl));
     }
 
-    @Test(expected = InvalidAffiliatedCompany.class)
-    public void testNonRegisteredAffiliatedCompanyThrowsException() throws Exception {
-        affiliationManager.constructAffiliatedLink(createNonRegisteredAffliatedCompany(), rawUrl);
-    }
-
-    @Test(expected = InvalidAffiliatedCompany.class)
-    public void testRegisteredAffiliatedCompanyThrowsExceptionIfNoAffiliatedIdIsIncluded() throws Exception {
-        affiliationManager.constructAffiliatedLink(createRegisteredNonAffliatedCompany(), rawUrl);
-    }
-
     @Test
-    public void testCustomUrlIsReceivedIfCompanyIsAffiliatedAndIdIsIncluded() throws Exception {
+    public void testCustomUrlIsReceivedIfCompanyIsAffiliated() throws Exception {
         String parsedUrl = affiliationManager.constructAffiliatedLink(createAmazonAffiliationCompany(), rawUrl);
         Assert.assertFalse(rawUrl.equals(parsedUrl));
         Assert.assertTrue(parsedUrl.indexOf(rawUrl) >= 0);
+    }
+
+    @Test
+    public void testCustomUrlIsReceivedWithMultipleKeysWhenCompanyIsAffiliated() throws Exception {
+        String parsedUrl = affiliationManager.constructAffiliatedLink(createDXAffiliationCompany(), dxRawUrl);
+        UrlParser urlParser = new UrlParser();
+        Map<String, List<String>> urlParameters = urlParser.extractParameters(parsedUrl);
+        String[] multipleAffiliateId = dxAffiliationIds.split(" ");
+        String[] multipleAffiliateIdKeys = dxAffiliationIdKeys.split(" ");
+        for (int i = 0; i < multipleAffiliateIdKeys.length; i++) {
+            Assert.assertTrue(multipleAffiliateId[i].equals(
+                    urlParameters.get(multipleAffiliateIdKeys[i]).get(0)));
+        }
     }
 
     @Test
@@ -68,18 +76,20 @@ public class AffiliationManagerTest {
     }
 
     protected OfferCompany createAmazonAffiliationCompany() {
-        return CompanyBuilderManager.aRegularCompanyWithName("Amazon").withAffiliateId(affiliateId).build();
+        return CompanyBuilderManager.aRegularCompanyWithName("Amazon")
+                .withAffiliateId(affiliateId)
+                .withAffiliateIdKey(affiliateIdKey).build();
     }
 
-    private OfferCompany createRegisteredNonAffliatedCompany() {
-        return CompanyBuilderManager.aRegularCompanyWithName("Amazon").withAffiliateId("").build();
-    }
-
-    private OfferCompany createNonRegisteredAffliatedCompany() {
-        return CompanyBuilderManager.aRegularCompanyWithName("Maxun").withAffiliateId("1213211").build();
+    protected OfferCompany createDXAffiliationCompany() {
+        return CompanyBuilderManager.aRegularCompanyWithName("Dealextreme")
+                .withAffiliateId(dxAffiliationIds)
+                .withAffiliateIdKey(dxAffiliationIdKeys).build();
     }
 
     private OfferCompany createNonAffliatedCompany() {
-        return CompanyBuilderManager.aRegularCompanyWithName("Maxun").withAffiliateId("").build();
+        return CompanyBuilderManager.aRegularCompanyWithName("Maxun")
+                .withAffiliateId("")
+                .withAffiliateIdKey("").build();
     }
 }
