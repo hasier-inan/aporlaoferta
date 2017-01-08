8e9380d92f78c13df62f91d5c3cfde6566e5a2fc
Hasier Iñan <hasiermetal@gmail.com>
2017-01-08 14:14:31 +0000

    Updated affiliations for tradetracker and Zanox

--- src/main/java/com/aporlaoferta/model/CompanyAffiliateType.java
+++ src/main/java/com/aporlaoferta/model/CompanyAffiliateType.java
@@ -4,5 +4,5 @@ package com.aporlaoferta.model;
  * Created by hasiermetal on 18/01/15.
  */
 public enum CompanyAffiliateType {
-    TRADEDOUBLER, BELBOON, TRADETRACKER, LINK
+    TRADEDOUBLER, BELBOON, TRADETRACKER, ZANOX, LINK
 }
